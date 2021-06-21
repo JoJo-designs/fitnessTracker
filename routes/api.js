a9e95116fcbd50601b296ed56c582165af58567f
jojo <jodiladouceur.design@gmail.com>
2021-06-21 11:13:24 -0400

    edits to the api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
-// const Exercises = require("../models/exercises.js");
+const Exercises = require("../models/exercises.js");
 
-
+// Attempt to get data from the database
+router.get('/api/data', (req, res) => {
+    Workout.find({})
+    .sort({ date: 1})
+    .then(Workouts => {
+        res.json(Workouts)
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 module.exports = router;
