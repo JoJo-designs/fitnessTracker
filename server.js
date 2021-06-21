101863bb1f38cfa34c587f75f1e6a9bfcdec1c4b
jojo <jodiladouceur.design@gmail.com>
2021-06-21 09:16:31 -0400

    edits to routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker"
 });
 
 // routes
-app.use(require("./controller/index.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
