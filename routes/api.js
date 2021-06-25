dd92e77e93557f06338bbdb9108712c258b9b945
jojo <jodiladouceur.design@gmail.com>
2021-06-25 12:20:00 -0400

    edits to the readme.md and routes.js

--- routes/api.js
+++ routes/api.js
@@ -12,12 +12,6 @@ router.get("/stats", function (req, res) {
   res.redirect('stats.html')
 })
 
-// if you get lost, look at api.js in public folder
-/**
- * make route for each of these calls and delete commented
- * code as you go
- */
-
 
 // gets all the data with the duration field added to the response.
 router.get("/api/workouts", (req, res) => {
@@ -71,21 +65,6 @@ router.post("/api/workouts", ({ body }, res) => {
 
 // doesn't work.
 router.get("/api/workouts/range", (req, res) => {
-  // Workout.aggregate([
-  //   {
-  //     $addfields: {
-  //       totalRange: {
-  //         $sum: '$exercises.distance',
-  //       },
-  //     },
-  //   },
-  // ])
-  // .then(workouts => {
-  //   res.json(workouts);
-  // })
-  // .catch(err => {
-  //   res.status(400).json(err);
-  // })
   Workout.aggregate([
     {
       $addFields: {
