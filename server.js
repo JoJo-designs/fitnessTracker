f6f9073988d0ee804354084f80584fb05def14db
jojo <jodiladouceur.design@gmail.com>
2021-06-26 14:37:42 -0400

    edits to pakeage json and routes api

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/fitnessTracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-  // useCreateIndex: true
 });
 
 // routes
